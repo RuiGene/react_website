cbb0b009ba2283e791d15d427e2dea15cb732333
RuiGene <zrdanielyang@gmail.com>
2023-12-04 21:11:25 +1300

    finished hero section

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -1,5 +1,25 @@
 export default function HeroSection() {
     return (
-        <h1>HeroSection</h1>
+        <section id="heroSection" className="hero--section">
+            <div className="hero--section--content--box">
+                <div className="hero--section-content">
+                    <p className="section--title">Hey, I'm Daniel</p>
+                    <h1 className="hero--section--title">
+                        <span className="hero--section--title--color">
+                            Full Stack 
+                        </span>{" "}
+                        <br />
+                        Developer
+                    </h1>
+                    <p className="hero--section-description">
+                        Lorem ipsum, dolor sit amet consectetu
+                        <br />
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    </p>
+                </div>
+                <button className="btn btn-primary">Get in touch</button>
+            </div>
+        </section>
     )
+    ;
 }
