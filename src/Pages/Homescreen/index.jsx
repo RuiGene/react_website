01cb2a0d591b4998ac6f7fd14ad8293b036eecbf
RuiGene <zrdanielyang@gmail.com>
2023-12-07 19:17:05 +1300

    Finished my skills section

--- src/Pages/Homescreen/index.jsx
+++ src/Pages/Homescreen/index.jsx
@@ -1,9 +1,11 @@
 import HeroSection from "../Home/HeroSection";
+import MySkills from "../Home/MySkills";
 
 export default function Home() {
     return (
         <>
         <HeroSection /> 
+        <MySkills />
         </>
     )
 }
