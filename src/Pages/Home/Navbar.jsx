6e9d475085082ade692450376ec21b51abacb4d5
RuiGene <zrdanielyang@gmail.com>
2023-12-07 17:12:45 +1300

    fixed not used issue

--- src/Pages/Home/Navbar.jsx
+++ src/Pages/Home/Navbar.jsx
@@ -1,12 +1,12 @@
 import { useState, useEffect } from "react";
-import { Link } from "react-scroll";
+// import { Link } from "react-scroll";
 
 function Navbar() {
   const [navActive, setNavActive] = useState(false);
 
-  const toggleNav = () => {
-    setNavActive(!navActive);
-  };
+  // const toggleNav = () => {
+  //   setNavActive(!navActive);
+  // };
 
   const closeMenu = () => {
     setNavActive(false);
