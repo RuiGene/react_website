887a8c2df663845c791ce57d629d43d62f7ec74a
RuiGene <zrdanielyang@gmail.com>
2023-12-09 12:27:25 +1300

    completed portfolio section

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,21 @@
+export default function AboutMe() {
+    return (
+        <section id="AboutMe" className="about--section">
+            <div className="about--section--img">
+            </div>
+            <div className="hero--section--content--box about--section--box">
+                <div className="hero--section--content">
+                    <p className="section--title">
+                        About
+                    </p>
+                    <h1 className="skills--section--heading">
+                        About Me
+                    </h1>
+                    <p className="hero--section-description">
+                        Lorem 3feowbfjief
+                    </p>
+                </div>
+            </div>
+        </section>
+    );
+}
--- src/Pages/Homescreen/index.jsx
+++ src/Pages/Homescreen/index.jsx
@@ -1,11 +1,15 @@
 import HeroSection from "../Home/HeroSection";
 import MySkills from "../Home/MySkills";
+import AboutMe from "../Home/AboutMe";
+import MyPortfolio from "../Home/MyPortfolio";
 
 export default function Home() {
     return (
         <>
         <HeroSection /> 
         <MySkills />
+        <AboutMe />
+        <MyPortfolio />
         </>
     )
 }
