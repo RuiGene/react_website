f8c9243185fe7ee43d057c37e19c7e7fbbe4321a
RuiGene <zrdanielyang@gmail.com>
2023-12-10 20:03:35 +1300

    added timeline

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -1,13 +1,8 @@
 export default function AboutMe() {
     return (
         <section id="AboutMe" className="about--section">
-            <div className="about--section--img">
-            </div>
             <div className="hero--section--content--box about--section--box">
                 <div className="hero--section--content">
-                    <p className="section--title">
-                        About
-                    </p>
                     <h1 className="skills--section--heading">
                         About Me
                     </h1>
--- src/Pages/Homescreen/index.jsx
+++ src/Pages/Homescreen/index.jsx
@@ -3,13 +3,15 @@ import MySkills from "../Home/MySkills";
 import AboutMe from "../Home/AboutMe";
 import MyPortfolio from "../Home/MyPortfolio";
 import Footer from "../Home/Footer";
+import TimelineHistory from "../Home/Testimonials";
 
 export default function Home() {
     return (
         <>
         <HeroSection /> 
-        <MySkills />
         <AboutMe />
+        <TimelineHistory />
+        <MySkills />
         <MyPortfolio />
         <Footer />
         </>
